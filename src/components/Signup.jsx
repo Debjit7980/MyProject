d0eee44bd6357d3c97bec6f39b9fcdbf655a399e
DebjitSinghaRoy <debjitsingharoy007@gmail.com>
2024-04-05 18:21:54 +0530

    files added

---
+++ src/components/Signup.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import image1 from './../assets/Images/signup.jpg';
+import { IoWarning } from "react-icons/io5";
+
+function Signup({ onClick }) {
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    const[email,setEmail]=useState('');
+    const [pass,setPass]=useState('');
+    const [agreed, setAgreed] = useState(false);
+    const [showMessage, setShowMessage] = useState(false);
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+        if (event.target.value === name) {
+            setShowMessage(true);
+        } else {
+            setShowMessage(false);
+        }
+    };
+
+    const handleEmailChange=(event)=>{
+        setEmail(event.target.value)
+    }
+
+    const handlePassChange=(event)=>{
+        setPass(event.target.value)
+    }
+
+    const handleCheckboxChange = () => {
+        setAgreed(!agreed);
+    };
+
+    return (
+        <div className='flex w-full'>
+            <div className='w-[33%] h-screen'>
+                <img src={image1} alt="image" className='w-full h-full' />
+            </div>
+            <div className='w-[45%] relative mx-auto'>
+                <span className='absolute top-0 -right-[20%] text-md'>Already a member? <span className='text-blue-600'>Sign In</span></span>
+                <div className='flex flex-col  mx-auto w-[70%]'>
+                    <span className='text-2xl pt-12 font-bold mr-auto'>Sign up to Dribbble</span>
+                    {showMessage && <span className='text-red-500 text-md font-bold mt-4'><li>Username has already been taken</li></span>}
+                    <div className='flex mt-10 text-md  w-full justify-between '>
+                        <span className='flex flex-col w-[45%]'>
+                            <span className='mr-auto font-bold'>Name</span>
+                            <input
+                                type='text'
+                                className='outline-0  rounded-lg p-2 bg-gray-200'
+                                value={name}
+                                onChange={handleNameChange}
+                            />
+                        </span>
+                        <span className='flex flex-col w-[45%]'>
+                            <span className='mr-auto font-bold flex gap-2'>{showMessage && <IoWarning className='text-xl text-red-500' />}Username</span>
+                            <input
+                                type='text'
+                                className={`outline-0 rounded-lg p-2 ${showMessage? 'bg-pink-100 text-red-500':'bg-gray-200'}`}
+                                value={username}
+                                onChange={handleUsernameChange}
+                            />
+                        </span>
+                    </div>
+                    <span className='text-md font-bold mr-auto mt-8'>Email</span>
+                    <input type="email" className='outline-0 p-2 bg-gray-200 rounded-lg' 
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                    <span className='text-md font-bold mr-auto mt-8'>Password</span>
+                    <input type="password" placeholder="6+ characters" className='outline-0  placeholder-gray-500 p-2 bg-gray-200 rounded-lg'
+                        value={pass}
+                        onChange={handlePassChange}
+                    />
+                    <div className='mt-6 flex'>
+                        <input type="checkbox" className='mb-auto h-6 w-6'
+                            checked={agreed}
+                            onChange={handleCheckboxChange}
+                        />
+                        <span className='text-sm ml-2 text-gray-500'>Creating an account means you're okay with our <span className='text-blue-600'>Terms of Service, Privacy Policy</span> and our default <span className='text-blue-600'>Notification Settings</span>.</span>
+                    </div>
+                   {!showMessage && name && username && email && pass && agreed ? (
+                        <button className='bg-pink-500 text-white pt-2 pb-2 mt-7 w-[40%] rounded-lg font-bold' onClick={onClick}>Create Account</button>
+                   ):(
+                        <button className='bg-pink-300 text-white pt-2 pb-2 mt-7 w-[40%] rounded-lg font-bold' disabled>Create Account</button>
+                   )}
+                    <span className='text-xs text-gray-500 w-[80%] mt-5'>This site is protected by reCAPTCHA and the Google <span className='text-blue-600'>Privacy Policy</span> and <span className='text-blue-600'>Terms of Service</span> apply.</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Signup;
